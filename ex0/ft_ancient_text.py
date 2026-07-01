3aabba35adccb9c87d43c016588a3ea27c9484a8
Denis Monseur <dmonseur@student.42belgium.be>
2026-07-01 16:53:18 +0200

    don t forget to del .txt

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def read_file() -> None:
+    if len(sys.argv) == 2:
+        print(f"Accessing file '{sys.argv[1]}'\n")
+        try:
+            file = sys.argv[1]
+            f = open(file)
+            print(f.read())
+            f.close()
+            print(f"File '{sys.argv[1]}' closed.")
+        except (FileNotFoundError) as e:
+            print(f"Error opening file '{sys.argv[1]}': {e}")
+        except PermissionError as e:
+            print(f"Error opening file '{sys.argv[1]}': {e}")
+    else:
+        print("Usage: ft_ancient_text.py <file>")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Recovery ===")
+    read_file()
